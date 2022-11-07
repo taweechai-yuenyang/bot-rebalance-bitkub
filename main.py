33daf2ef254f618a6429a947bf57dab5405d07ab
abe27 <krumii.it@gmail.com>
2022-11-07 16:32:42 +0700

    update log

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ header = {
 
 def create_log(msg):
     d = datetime.now()
-    fileName = f"rebalance/logs/{d.strftime('%Y-%m-%d')}-log.txt"
+    fileName = os.path.join(os.path.dirname(__file__),f"logs/{d.strftime('%Y-%m-%d')}-log.txt")
     with open(fileName, "a") as f:
         # Append 'hello' at the end of file
         f.write(f"{d.strftime('%Y-%m-%d %H:%M:%S')} {msg}\n")
